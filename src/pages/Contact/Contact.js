e8189cdf34913eeda3072e7d50d902ffbe12b6d4
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-05-08 22:09:48 +0530

    footer component finished

---
+++ src/pages/Contact/Contact.js
@@ -0,0 +1,14 @@
+import Layout from "../../layout/Layout";
+import Container from "../../components/UI/Container/Container";
+
+function Contact() {
+  return (
+    <Layout>
+      <Container>
+        <h1>Contact Page</h1>
+      </Container>
+    </Layout>
+  );
+}
+
+export default Contact;
