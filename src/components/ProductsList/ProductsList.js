50127ed41da4dfa7c28bcbe5971135aca97290d1
Krutagna Patel <kpatel.kp7016@gmail.com>
2023-05-09 19:33:37 +0530

    added product component

--- src/components/ProductsList/ProductsList.js
+++ src/components/ProductsList/ProductsList.js
@@ -1,29 +1,35 @@
 import Section from "../UI/Section/Section";
 import Container from "../UI/Container/Container";
 import "./ProductList.css";
+import { IonIcon } from "@ionic/react";
+import { cart } from "ionicons/icons";
 
 function ProductsList({ products }) {
   return (
     <Section className="products-section">
-      <Container className="products grid grid--gap">
-        {products.map((product) => (
-          <div className="product">
-            <div className="product__image-wrapper">
-              <img
-                className="product__image"
-                src={product.image}
-                alt={product.title}
-              />
+      <Container className="products">
+        <h1 className="products__heading">Our Products</h1>
+        <div className="products__content grid grid--gap">
+          {products.map((product) => (
+            <div className="product">
+              <div className="product__image-wrapper">
+                <img
+                  className="product__image"
+                  src={product.image}
+                  alt={product.title}
+                />
+              </div>
+              <div className="product__content grid grid--items-center grid--gap">
+                <h3 className="product__title">{product.title}</h3>
+                <p className="product__price">Price: &#8377;{product.price}</p>
+                <button className="product__btn btn btn--peach flex flex--align-center flex--gap">
+                  <IonIcon className="product__btn-icon" icon={cart} />
+                  Add to Cart
+                </button>
+              </div>
             </div>
-            <div className="product__content grid grid--items-center grid--gap">
-              <h3 className="product__title">{product.title}</h3>
-              <p className="product__price">Price: &#8377;{product.price}</p>
-              <button className="product__btn btn btn--peach">
-                Add to Cart
-              </button>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </Container>
     </Section>
   );
